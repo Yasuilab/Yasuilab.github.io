b5853d1eb1fb7fb5353e0d12cdf7a7ef7d34794e
cehl-kurage <83456982+cehl-kurage@users.noreply.github.com>
2026-04-05 06:53:20 +0000

    Deploying to gh-pages from @ cehl-kurage/labhp.github.io@6f856b13ef0e7d20078bb505834a47fa98077e24 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "cehl-kurage/LabHP",
+    "data-repo": "cehl-kurage/labhp.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
